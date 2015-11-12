5b2b62fb5b39271236bdeb8c64914836ad69f996
fenvenfen <volodymer51558@gmail.com>
2015-11-12 13:57:03 +0100

    start make grunt task

---
+++ grunt-swagger-ng-ts/Gruntfile.js
@@ -0,0 +1,20 @@
+﻿module.exports = function (grunt) {
+    'use strict';
+    grunt.initConfig({
+        smkdir: {
+            noop: {},
+            simple: {
+                options: {
+                    create: ['tmp'],
+                    swagger: 'swagger/swagger.json',
+                    content: 'Volodymyr Yeryhin',
+                    fileName: 'single.html'
+                }
+            }
+        }
+    });
+    
+    // Load local tasks.
+    grunt.loadTasks('tasks');
+    grunt.registerTask('TsCodegen', ['smkdir']);
+};
---
+++ grunt-swagger-ng-ts/tasks/swagger-ts-codegen.ts
@@ -0,0 +1,26 @@
+﻿module.exports = function (grunt) {
+    'use strict';
+    var fs = require('fs');
+
+    grunt.registerMultiTask('smkdir', 'Log stuff.', function () {
+        var options;
+        options = this.options({
+            mode: null,
+            create: [],
+            content: null,
+            swagger: null,
+            fileName: null
+        });
+      
+        return options.create.forEach(function (filepath, fileName, content) {
+            grunt.log.write('Creating "' + filepath + '"...');
+                fileName = options.fileName;
+                filepath = grunt.template.process(filepath);
+                grunt.file.mkdir(filepath, options.mode);
+                fs.writeFileSync(filepath + '/' + fileName, content, 'UTF-8');
+               // grunt.file.write(fileName, options.content);
+                return grunt.log.ok();        
+        });
+    });
+};
+
